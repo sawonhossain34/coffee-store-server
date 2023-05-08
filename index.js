a0b088607122eae9b2f191df933a5980f78a3f68
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-08 19:14:43 +0600

    coffee update set done

--- index.js
+++ index.js
@@ -52,6 +52,26 @@ async function run() {
       res.send(result);
     })
 
+    app.put('/coffee/:id' ,async (req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert : true}
+      const updatedCoffee = req.body;
+      const coffee ={
+        $set: {
+          name: updatedCoffee.name,
+          quentity: updatedCoffee.quentity,
+          supplier: updatedCoffee. supplier,
+          taste: updatedCoffee. taste,
+          category: updatedCoffee. category,
+          details: updatedCoffee. details,
+          photo: updatedCoffee. photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter,coffee,options);
+      res.send(result)
+    })
+
     app.delete('/coffee/:id',async(req,res) => {
       const id = req.params.id;
       const quary = {_id: new ObjectId(id)}
